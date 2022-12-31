786c0c25a64ea2c0b164b7e5ed6b2641908f429b
hejurado@gmail.com <root@NACPOP4GPPED1.localdomain>
2022-12-31 11:09:49 -0500

    creating users and circles

--- cride/circles/models/circles.py
+++ cride/circles/models/circles.py
@@ -8,9 +8,10 @@ from cride.utils.models import CRideModel
 
 
 class Circle (CRideModel):
-    """Circle model.
+    """Circle model. this is my test
     A  circle is a private group where rides are offered and taken
      by its members.  To join a circle  a user must receive an unique
      invitation code from an existing circle member
     """
 
+        
